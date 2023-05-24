de93d4e419c0c874bb0895bd07e3c1a468ccb754
snehanr <134271122+snehanr@users.noreply.github.com>
2023-05-24 11:14:42 +0530

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class MyFirstJavaProgram {
 	   public static void main(String []args) {
-		         System.out.println("Hello World"); // prints Hello World
+		         System.out.println("Hello World !!"); // prints Hello World
 	   }	
 }
